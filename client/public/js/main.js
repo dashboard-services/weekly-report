06e057e5f4ffeccd4e9bf997ed2f80b6bc15733d
Ignacio Tolstoy <arkantos798@gmail.com>
2014-11-10 18:13:52 +0100

    Test responsive chart

--- client/public/js/main.js
+++ client/public/js/main.js
@@ -5,59 +5,44 @@ $(function() {
 });
 
 function buildChart(data) {
-	AmCharts.makeChart("chartdiv",
-		{
-			"type": "serial",
-			"pathToImages": "//cdn.amcharts.com/lib/3/images/",
-			"categoryField": "category",
-			"startDuration": 1,
-			"categoryAxis": {
-				"gridPosition": "start"
+	AmCharts.makeChart("chartdiv", {
+		"type": "serial",
+		"theme": "light",
+		"pathToImages": "//cdn.amcharts.com/lib/3/images/",
+		"categoryField": "category",
+		"startDuration": 1,
+		"categoryAxis": {
+			"gridPosition": "start"
+		},
+		"trendLines": [],
+		"graphs": [
+			{
+				"fillAlphas": 1,
+				"id": "AmGraph-1",
+				"title": "Sent emails",
+				"type": "column",
+				"valueField": "sent"
 			},
-			"trendLines": [],
-			"graphs": [
-				{
-					"balloonText": "[[title]] of [[category]]:[[value]]",
-					"fillAlphas": 1,
-					"id": "AmGraph-1",
-					"title": "Sent emails",
-					"type": "column",
-					"valueField": "sent"
-				},
-				{
-					"balloonText": "[[title]] of [[category]]:[[value]]",
-					"fillAlphas": 1,
-					"id": "AmGraph-2",
-					"title": "Delivered emails",
-					"type": "column",
-					"valueField": "delivered"
-				},
-				{
-					"id": "AmGraph-3",
-					"title": "Open emails",
-					"valueField": "open"
-				}
-			],
-			"guides": [],
-			"valueAxes": [
-				{
-					"id": "ValueAxis-1",
-					"title": "N. of emails"
-				}
-			],
-			"allLabels": [],
-			"balloon": {},
-			"legend": {
-				"useGraphSettings": true
+			{
+				"fillAlphas": 1,
+				"id": "AmGraph-2",
+				"title": "Delivered emails",
+				"type": "column",
+				"valueField": "delivered"
 			},
-			"titles": [
-				{
-					"id": "Title-1",
-					"size": 15,
-					"text": "Weekly report data"
-				}
-			],
-			"dataProvider": data
-		}
-	);
+			{
+				"id": "AmGraph-3",
+				"title": "Open emails",
+				"valueField": "open"
+			}
+		],
+		"guides": [],
+		"valueAxes": [{
+			"axisAlpha": 0,
+			"minimum": 0,
+		}],
+		"allLabels": [],
+		"balloon": {},
+		"dataProvider": data
+	});
 }
